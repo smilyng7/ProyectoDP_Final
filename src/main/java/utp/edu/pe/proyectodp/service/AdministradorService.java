d2d23f8657e8bc30e6a6fb57de63bd2dd8a8a48e
Evelyn <evidendz@gmail.com>
2026-06-28 21:53:42 -0500

    Update AdministradorService.java
    actualizacion

--- src/main/java/utp/edu/pe/proyectodp/service/AdministradorService.java
+++ src/main/java/utp/edu/pe/proyectodp/service/AdministradorService.java
@@ -4,7 +4,17 @@ import utp.edu.pe.proyectodp.entity.Administrador;
 
 import java.util.List;
 import java.util.Optional;
-
+/**
+ * Implementación del patrón Pattern para la generación de códigos únicos.
+ *
+ * <p>Esta clase garantiza la existencia de una única instancia durante
+ * toda la ejecución del sistema, permitiendo generar códigos consecutivos
+ * para matrículas, pagos, cursos y otros módulos.</p>
+ *
+ * @author Evelyn
+ * @version 1.0
+ * @since 2026
+ */
 public interface AdministradorService {
 
     List<Administrador> listar();
